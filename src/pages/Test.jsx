f2d2acdd05c57e6c3d5c2d1b5e545159d15b466c
MorasDeFresa <juancastromora@proton.me>
2025-09-23 19:08:21 -0500

    🐞 fix: Delete unused import

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,11 +1,7 @@
-import { useRandomNumber } from "../hooks/useRandomNumber";
-
 const Test = () => {
-  const randomNumber = useRandomNumber((state) => state.numbers);
   return (
     <>
       <p>A</p>
-      <p>{randomNumber}</p>
     </>
   );
 };
