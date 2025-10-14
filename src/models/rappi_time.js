afe7af0bfa9268b4f45808a411e453960dd05f01
MorasDeFresa <bencito10@gmail.com>
2025-10-14 15:38:10 -0500

    🐞 fix: Distribution of time

--- src/models/rappi_time.js
+++ src/models/rappi_time.js
@@ -1,19 +1,22 @@
+import { exponencial } from "./exponencial";
+
 export function rappi_time(
   r,
   AgrupacionPorOrden = 5,
   BonificacionPedido = 0.1,
   IndiceGanancia = 0.5,
   GananciaBase = 4000,
-  CostoBase = 2000,
-  TiempoBase = 300
+  CostoBase = 5000,
+  TiempoBase = 300,
+  MediaSegundos = 600
 ) {
   const rCopy = [...r];
-
+  const lambda = 1 / (MediaSegundos / 60);
   const CostoTotal = [];
   if (!Array.isArray(rCopy) || rCopy.length === 0) return [];
 
   for (let index = 0; index < rCopy.length; index++) {
-    rCopy[index] = TiempoBase + TiempoBase * rCopy[index];
+    rCopy[index] = 300 + exponencial(lambda, rCopy[index]);
     let aux =
       ((rCopy[index] - TiempoBase) / AgrupacionPorOrden) *
       (BonificacionPedido * (IndiceGanancia * GananciaBase));
