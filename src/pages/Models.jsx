4d2f244264bf30485d959f75b30040280c995afd
MorasDeFresa <bencito10@gmail.com>
2025-10-15 17:22:22 -0500

    🖥️ wip: csv downloader component

--- src/pages/Models.jsx
+++ src/pages/Models.jsx
@@ -7,6 +7,7 @@ import { redirect } from "react-router-dom";
 import DeliveryChart from "../components/DeliveryChart";
 import TimeByClientDensityChart from "../components/TimeByClientDensityChart";
 import BoxPlot from "../components/Boxplot";
+import CsvDownloader from "react-csv-downloader";
 
 const Models = () => {
   const r = useBear((state) => state);
@@ -30,26 +31,26 @@ const Models = () => {
   }
   return (
     <>
-    <div className="context-card">
-      <h1>Modelos y simulación</h1>
-      <h2>Simulacion Costos de Entrega</h2>
-      <h3>Contexto</h3>
-      <p>
-        El modelo se centra en el proceso al momento que un usuario realiza un
-        pedido mediante una aplicacion de domicilios, como se procesa este
-        pedido y teniendo en cuenta las politicas de la app y el tiempo se
-        establece una simulacion del coste variable de cada uno de los pedidos
-      </p>
-      <h3>Variables</h3>
-      <ol>
-        <li>𝜀𝑜: 50% de la ganancia estimada de la orden</li>
-        <li>
-          ∆𝑡 Diferencia de tiempo desde el lanzamiento de la orden y aceptación
-          de la orden.
-        </li>
-        <li>∆𝑓 Evento de agrupación y despachar orden</li>
-      </ol>
-    </div>
+      <div className="context-card">
+        <h1>Modelos y simulación</h1>
+        <h2>Simulacion Costos de Entrega</h2>
+        <h3>Contexto</h3>
+        <p>
+          El modelo se centra en el proceso al momento que un usuario realiza un
+          pedido mediante una aplicacion de domicilios, como se procesa este
+          pedido y teniendo en cuenta las politicas de la app y el tiempo se
+          establece una simulacion del coste variable de cada uno de los pedidos
+        </p>
+        <h3>Variables</h3>
+        <ol>
+          <li>𝜀𝑜: 50% de la ganancia estimada de la orden</li>
+          <li>
+            ∆𝑡 Diferencia de tiempo desde el lanzamiento de la orden y
+            aceptación de la orden.
+          </li>
+          <li>∆𝑓 Evento de agrupación y despachar orden</li>
+        </ol>
+      </div>
       <DeliveryChart
         data={result[0].map((valor, idx) => ({
           index: idx + 1,
@@ -111,23 +112,25 @@ const Models = () => {
       </div>
       <br />
       <div className="context-card">
-      <h2>Simulación de Tiempos de Atención por Cliente (Modelo de Poisson)</h2>
-      <h3>Contexto</h3>
-      <p>
-        Funcionamiento de un cajero de supermercado, que es una situación muy
-        común donde los clientes llegan de forma aleatoria y deben ser atendidos
-        uno por uno, el modelo sirve para analizar cuántos clientes llegan en
-        determinado tiempo y cuánto se demora en atender a cada uno.
-      </p>
-      <h3>Variables</h3>
-      <ol>
-        <p>Poisson llegadas de clientes:</p>
-        <li>λ (lambdaLlegadas)</li>
-        <li>k (clientes)</li>
-        <p>Exponencial tiempo de servicio</p>
-        <li>λ (lambdaServicio)</li>
-        <li>T (tiempo de servicio)</li>
-      </ol>
+        <h2>
+          Simulación de Tiempos de Atención por Cliente (Modelo de Poisson)
+        </h2>
+        <h3>Contexto</h3>
+        <p>
+          Funcionamiento de un cajero de supermercado, que es una situación muy
+          común donde los clientes llegan de forma aleatoria y deben ser
+          atendidos uno por uno, el modelo sirve para analizar cuántos clientes
+          llegan en determinado tiempo y cuánto se demora en atender a cada uno.
+        </p>
+        <h3>Variables</h3>
+        <ol>
+          <p>Poisson llegadas de clientes:</p>
+          <li>λ (lambdaLlegadas)</li>
+          <li>k (clientes)</li>
+          <p>Exponencial tiempo de servicio</p>
+          <li>λ (lambdaServicio)</li>
+          <li>T (tiempo de servicio)</li>
+        </ol>
       </div>
       <TimeByClientDensityChart
         data={result[1][4].map((valor, idx) => ({
@@ -135,7 +138,6 @@ const Models = () => {
           valor: Number(valor),
         }))}
       />
-
       {result[1] && (
         <div style={{ marginTop: "24px" }}>
           <h3>Análisis de resultados</h3>
@@ -179,6 +181,14 @@ const Models = () => {
         </div>
       )}
       <BoxPlot results={result[1][4]} />
+      <CsvDownloader
+        filename="Test"
+        datas={[
+          { id: 1, test: result[1][4][0] },
+          { id: 1, test: result[1][4][1] },
+        ]}
+        text="Descargar simulaciones"
+      />
     </>
   );
 };
